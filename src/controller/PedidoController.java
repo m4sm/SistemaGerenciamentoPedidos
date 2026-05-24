85be36b4567f2f0f646ee4d9090a94ee03dfc35f
Arthurzão <arthborgestv3333@gmail.com>
2026-05-24 20:22:23 -0300

    Criando Model, Repository e Controller de Cliente

--- src/controller/PedidoController.java
+++ src/controller/PedidoController.java
@@ -4,10 +4,28 @@
  */
 package controller;
 
-/**
- *
- * @author marco
- */
+import model.Pedido;
+import java.util.ArrayList;
+
 public class PedidoController {
     
+    public void incluirPedido(Pedido p){
+        p.incluir();
+    }
+    
+    public void alterarPedido(Pedido p){
+        p.alterar();
+    }
+    
+    public Pedido consultarPedido(int id){
+        return Pedido.consultar(id);
+    }
+    
+    public void excluirPedido(int id){
+        Pedido.excluir(id);
+    }
+    
+    public ArrayList<Pedido> listarPedido(){
+        return Pedido.listar();
+    }
 }
--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -8,6 +8,85 @@ package model;
  *
  * @author marco
  */
+
+import java.time.LocalDate;
+import repository.PedidoRepository;
+import java.util.ArrayList;
+
 public class Pedido {
+    public int id_pedido;
+    public int id_cliente;
+    public LocalDate data_entrega;
+    public LocalDate data_pedido;
+    public float valor_total;
     
+    private static PedidoRepository repository = new PedidoRepository();
+    
+    //Construtores
+    public Pedido(int id_pedido, int id_cliente, LocalDate data_entrega, LocalDate data_pedido, float valor_total){
+        this.id_pedido = id_pedido;
+        this.id_cliente = id_cliente;
+        this.data_entrega = data_entrega;
+        this.data_pedido = data_pedido;
+        this.valor_total = valor_total;
+    }
+    
+    //Getters
+    public int getIdPedido(){
+        return id_pedido;
+    }
+    
+    public int getIdCliente(){
+        return id_cliente;
+    }
+    
+    public LocalDate getDataEntrega(){
+        return data_entrega;
+    }
+    
+    public LocalDate getDataPedido(){
+        return data_pedido;
+    }
+    
+    public float getValorTotal(){
+        return valor_total;
+    }
+    
+    //Setters
+    public void setIdPedido(int id_pedido){
+        this.id_pedido = id_pedido;
+    }
+    
+    public void setIdCliente(int id_cliente){
+        this.id_cliente = id_cliente;
+    }
+    
+    public void setDataEntrega(LocalDate data_entrega){
+        this.data_entrega = data_entrega;
+    }
+    
+    public void setDataPedido(LocalDate data_pedido){
+        this.data_pedido = data_pedido;
+    }
+    
+    //Funções
+    public void incluir(){
+        repository.incluir(this);
+    }
+    
+    public void alterar(){
+        repository.alterar(this);
+    }
+    
+    public static Pedido consultar(int id){
+       return  repository.consultar(id);
+    }
+    
+    public static void excluir(int id){
+        repository.excluir(id);
+    }
+    
+    public static ArrayList<Pedido> listar(){
+        return repository.listar();
+    }
 }
