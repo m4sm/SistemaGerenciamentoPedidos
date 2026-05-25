14b9453699f8dec81cf151cf4457b9562fde916f
guilherme <guilherme.souza@aluno.ueg.br>
2026-05-24 21:45:14 -0300

    Model finalizada

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author marco
- */
 public class Produto {
-    
+    private int codProduto;
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+
+    // Construtor Vazio
+    public Produto() {
+    }
+
+    // Construtor Completo (Corrigido para o Controller usar)
+    public Produto(int codProduto, String nome, double preco, int quantidadeEstoque) {
+        this.codProduto = codProduto;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    // Métodos Getters e Setters
+    public int getCodProduto() {
+        return codProduto;
+    }
+
+    public void setCodProduto(int codProduto) {
+        this.codProduto = codProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "codProduto=" + codProduto + ", nome=" + nome + ", preco=" + preco + ", quantidadeEstoque=" + quantidadeEstoque + '}';
+    }
 }
