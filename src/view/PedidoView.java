4c04130dc8ca382990dd55b6c7e2fb0b3de7f167
m4sm <marcoantoniosousamiranda@gmail.com>
2026-05-27 20:25:06 -0300

    Implementando os eventos Consultar, Excluir e Alterar

--- src/view/PedidoView.java
+++ src/view/PedidoView.java
@@ -8,7 +8,7 @@ package view;
 public class PedidoView extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(PedidoView.class.getName());
-    -
+    
     public PedidoView() {
         initComponents();
     }
